d9b0560fece3d459caaba96e9179d9575e936a7e
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-26 05:39:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+In `index.js`, build an ES2015 `BoardMember` class with the following properties set by the constructor: `name`, `homeState`, and `training`.
+
+Add the following methods to the prototype using the class syntax:
++ `veto` — returns `No, I must disagree`
+
++ `approve` — returns `You can do that!`
+
++ `doCharity` — returns `I like to help people.`
+
++ `releasePressStatement` — returns `You will see great things from Scuber.`
+
++ `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+}

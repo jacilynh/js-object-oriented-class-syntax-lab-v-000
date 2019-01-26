7090d34b1cc4e5f52f940641bdfed28dd8828642
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-26 05:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ class BoardMember {
   }
   
   sayHi() {
-    return `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+    return `"Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
   }
 }
